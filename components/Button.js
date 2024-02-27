7428132ec7added7549e4f02fe746007753d7ff4
ethanmcf <ethan_mcfarland@outlook.com>
2024-02-26 22:24:36 -0500

    Added animations to header

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,23 @@
 "use client";
+import Arrow from "@/svgs/arrow-right-solid.svg";
+import Image from "next/image";
+
 export default function Button(props) {
   return (
     <button
       className={
-        "bg-primary text-white h-14 flex items-center pl-10 pr-10 rounded-full shadow-xl shadow-blue-100"
+        "bg-primary text-white h-14 flex items-center pl-10 pr-10 " +
+        "rounded-full shadow-xl shadow-blue-100 hover:scale-95 transform duration-100 group"
       }
     >
-      <text className={"font-semibold"}>
-        {props.children} {">"}
-      </text>
+      <text className={"font-semibold"}>{props.children}</text>
+      <Image
+        src={Arrow}
+        alt={"arrow"}
+        className={
+          "ml-2 h-5 w-5 group-hover:translate-x-2 transform duration-150"
+        }
+      />
     </button>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -8,13 +8,15 @@ export default function Header() {
         "max-w-6xl w-full items-center justify-between text-xl lg:flex text-tBlack "
       }
     >
-      <div className={"flex items-center"}>
+      <button
+        className={"flex items-center hover:scale-95 transform duration-100"}
+      >
         <Image src={Logo} alt={"Logo"} className="w-8 h-8" />
         <text className={"font-semibold text-3xl pl-2"}>Healthcare</text>
-      </div>
+      </button>
       <div className={"flex space-x-5 items-center text-tBlack"}>
-        <text>Home</text>
-        <text>About</text>
+        <button className={"hover:text-primary"}>Home</button>
+        <button className={"hover:text-primary"}>About</button>
         <Button>Connect</Button>
       </div>
     </div>
