9ad8d27b90f60d5056002771c851eea34881100f
ethanmcf <ethan_mcfarland@outlook.com>
2024-02-26 21:15:10 -0500

    Updated file paths and added directories

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-xl lg:flex">
+    <main className="flex min-h-screen flex-col  bg-red-600 items-center justify-between pt-10">
+      <div className=" max-w-6xl w-full items-center justify-between text-xl lg:flex bg-blue-300">
         ULA IS COMING SOON
       </div>
     </main>
